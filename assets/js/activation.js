7d32d3e57eacc004725df1fbff68933ab46ca315
Bill Hinderman <bill@billhinderman.com>
2016-12-20 13:13:46 -0600

    Contact us page

--- assets/js/activation.js
+++ assets/js/activation.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   ajaxFormSpreeForm($('.activation-mailer form'), $('.activation-mailer #mailer-response'));
+  ajaxFormSpreeForm($('.contact-mailer form'), $('.activation-mailer #mailer-response'));
 
   function ajaxFormSpreeForm($form, $resultElement) {
     $form.submit(function(e) {
@@ -28,21 +29,21 @@ function submitFormspreeForm($form, $resultElement) {
     dataType: 'json',
     beforeSend: function() {
       $resultElement.empty().removeClass('error success').addClass('attempting');
-      $('.activation-mailer form').removeClass('error success').addClass('attempting');
+      $form.removeClass('error success').addClass('attempting');
       $resultElement.html("Sending.");
     },
     success: function(data) {
       $resultElement.empty().removeClass('error attempting').addClass('success');
-      $('.activation-mailer form').removeClass('error attempting').addClass('success');
+      $form.removeClass('error attempting').addClass('success');
       $resultElement.html("Your message has been sent! We’ll respond to your inquiry within 24 hours.");
       setTimeout(function() {
-        $('.activation-mailer').html("<h2>Thank you</h2><p>Your message has been sent! We’ll respond to your inquiry within 24 hours.</p>");
+        $form.html("<h2>Thank you</h2><p>Your message has been sent! We’ll respond to your inquiry within 24 hours.</p>");
       },2000);
     },
     error: function(err) {
       var message = "Sorry. Unable to send at this time. Please try again later.";
       $resultElement.empty().removeClass('success attempting').addClass('error');
-      $('.activation-mailer form').removeClass('success attempting');
+      $form.removeClass('success attempting');
       $resultElement.html(message);
     }
   });
